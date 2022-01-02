84db38e00e1325005adada50beafd82ddafa49bd
Nicolas Carval <nicolascarval@yahoo.fr>
2022-01-03 00:21:08 +0100

    Updating README with final API screenshot and conclusions, removal of unused code in app.py, updating sstyle of the API

--- app.py
+++ app.py
@@ -14,12 +14,14 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
 
     int_features = [x for x in request.form.values()]
+    month_value = int_features.pop()
+    int_features.insert(7,month_value)
     if int_features[-1] == "Autumn":
         int_features.pop()
         int_features.extend([1,0,0,0])
@@ -34,22 +36,14 @@ def predict():
         int_features.extend([0,0,0,1])
     
     int_features = [int(int_features[x]) if x not in [1,3] else float(int_features[x]) for x in range(len(int_features))]
-    for elem in int_features:
-        print(elem,' de type:',type(elem))
+    
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0])
 
-    return render_template('index2.html', prediction_text='Number of bike to be rented should be {0}'.format(output))
+    return render_template('index.html', prediction_text='Number of bike to be rented should be {0}'.format(output))
 
-#@app.route('/results',methods=['POST'])
-#def results():
-#
-#    data = request.get_json(force=True)
-#    prediction = model.predict([np.array(list(data.values()))])
-#    output = prediction[0]
-#    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8989)
